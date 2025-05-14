051f54e2f413f7d1f748f16ef547b5eb4215b188
Mary <masha.mashchalkina@mail.ru>
2025-05-14 17:48:09 +0300

    добавлен список отделов отдельным файлом

---
+++ frontend/vacation-calendar/src/components/constants.js
@@ -0,0 +1,9 @@
+// отделы
+export const DEPARTMENTS = [
+  { id: 'dev', name: 'Разработка' },
+  { id: 'qa', name: 'Тестирование' },
+  { id: 'marketing', name: 'Маркетинг' },
+  { id: 'hr', name: 'HR' },
+  { id: 'sales', name: 'Продажи' },
+  { id: 'support', name: 'Поддержка' }
+];
